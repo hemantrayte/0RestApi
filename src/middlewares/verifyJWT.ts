27e7dede621b6e65fd243420e6b2b2ce34269cdb
hemant <hemantrayte25@gmail.com>
2025-09-22 08:14:03 +0530

    add authentication middleware

--- src/middlewares/verifyJWT.ts
+++ src/middlewares/verifyJWT.ts
@@ -1,31 +1,31 @@
-// import { NextFunction, Request, Response } from "express";
-// import createHttpError from "http-errors";
-// import jwt from 'jsonwebtoken'
-// import userModel from "../user/user.model";
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import jwt from 'jsonwebtoken'
+import userModel from "../user/user.model";
 
-// export interface AuthRequest extends Request {
-//   userId:string
-// }
+export interface AuthRequest extends Request {
+  userId:string
+}
 
-// const verifyJWT = async(req:Request,res:Response, next:NextFunction) => {
+const verifyJWT = async(req:Request,res:Response, next:NextFunction) => {
 
-//   const token =
-//   req.cookies?.accessToken ||
-//   req.header("Authorization")?.replace("Bearer ", "");
+  const token =
+  req.cookies?.accessToken ||
+  req.header("Authorization")?.replace("Bearer ", "");
 
-//   if(!token) {
-//     const error= createHttpError(401,"Token Not Exist")
-//     next(error)
-//   }
+  if(!token) {
+    const error= createHttpError(401,"Token Not Exist")
+    next(error)
+  }
 
-//   const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string);
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string);
 
-//   const _req = req as AuthRequest
+  const _req = req as AuthRequest
 
-//   _req.userId = decodedToken.sub as string
+  _req.userId = decodedToken.sub as string
 
-//   next();
+  next();
 
-// }
+}
 
-// export default verifyJWT
+export default verifyJWT
